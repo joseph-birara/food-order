7cb857ceeff069ce2dad308610eeb29014fe4f7a
joseph-birara <josephbirara92@gmail.com>
2022-05-29 18:34:40 +0300

    all file structures created

---
+++ admin/partials/footer.php
@@ -0,0 +1,31 @@
+<!-- Footer Section Starts -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title></title>
+    <style>
+        .footer{
+            background-color: #514d4d;
+            position: fixed;
+            bottom: 0%;
+            width:100%;
+            background-attachment: scroll;
+            display:block;
+
+        }
+    </style>
+</head>
+
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">&copy2022 All rights reserved </p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
